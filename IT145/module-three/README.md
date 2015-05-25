df1f0699851aaa4286fb72bd68dfa9fa07f2cdaa
Sean M. Snyder <sean.snyder@snhu.edu>
2015-05-25 13:56:48 -0400

    week 3 IT-145

---
+++ IT145/module-three/README.md
@@ -0,0 +1,6 @@
+# Module 3
+
+### Problem 3-3
+
+Prompt the user to enter three numbers and display them in
+non-descending order.
---
+++ IT145/module-three/ReturnThreeNonDescending.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class ReturnThreeNonDescending {
+  JOptionPane prompt, display;
+  double[] inputNumbers, sortedNumbers;
+
+  public double[] reverseSort(double[] list) {
+    for( double item : list) {
+    }
+
+   return sortedNumbers; 
+  }
+
+  public static void main(String[] args) {
+
+  }
+}

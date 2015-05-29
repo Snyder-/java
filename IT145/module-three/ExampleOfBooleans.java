e01a5d1f016e4edcb863ceb0ecd3c489d992baf2
Sean M. Snyder <sean.snyder@snhu.edu>
2015-05-29 14:18:07 -0400

    add example for classmate

---
+++ IT145/module-three/ExampleOfBooleans.java
@@ -0,0 +1,15 @@
+class ExampleOfBooleans {
+  public static void main (String[] args) {
+   Boolean x = false;
+   if (x) {
+     System.out.println("if(x) executed...");
+   }
+   if(!x) {
+   System.out.println("if(!x) executed...");
+   }
+   if(x == false) {
+     System.out.println("if(x == false) executed...");
+   }
+   System.exit(0);
+  }
+}

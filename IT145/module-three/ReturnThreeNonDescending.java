1cc6f39bdc65f8d6329eb55627424b670f234e11
Sean M. Snyder <sean.snyder@snhu.edu>
2015-05-30 09:18:00 -0400

    exception handling

--- IT145/module-three/ReturnThreeNonDescending.java
+++ IT145/module-three/ReturnThreeNonDescending.java
@@ -1,4 +1,5 @@
 /* Sean Snyder - IT 145 - Return three inputed numbers in non-descending order
+ * May 26, 2015
  *
  * Program prompts the user to enter three numbers to be sorted. Each string response is
  * parsed into an integer and stored in an integer array.  The array is sorted and the user
@@ -14,12 +15,16 @@ import java.util.*;
 public class ReturnThreeNonDescending {
   public ReturnThreeNonDescending() {
     String prompt;
-    int firstNum, secondNum, thirdNum;
+    int confirm, firstNum, secondNum, thirdNum;
 
-    JOptionPane.showConfirmDialog(null,
+    confirm = JOptionPane.showConfirmDialog(null,
                      "This program takes three numbers and sorts them in non-descending order.\nDo you want to continue?",
                      "Sort Three Numbers - Sean Snyder - IT 145", JOptionPane.YES_NO_OPTION);
+    if(confirm == 1) {
+      System.exit(0);
+    }
 
+    try {
     prompt = JOptionPane.showInputDialog("Please enter the first number:");
     firstNum = parseUserInput(prompt);
 
@@ -29,15 +34,29 @@ public class ReturnThreeNonDescending {
     prompt = JOptionPane.showInputDialog("Enter the third number:");
     thirdNum = parseUserInput(prompt);
 
-   int[] inputArray = { firstNum, secondNum, thirdNum };
+    int[] inputArray = { firstNum, secondNum, thirdNum };
 
     Arrays.sort(inputArray);
 
-    JOptionPane.showConfirmDialog(null,inputArray[0] + "\n" + inputArray[1] + "\n" + inputArray[2],
+    JOptionPane.showConfirmDialog(null,getList(inputArray),
         "Sorted Results", JOptionPane.DEFAULT_OPTION);
+
+    System.exit(0);
+    }catch(Exception e) {
+      System.out.println("An error occured");
+      System.exit(0);
+    }
   }
 
-  public static int parseUserInput(String input) {
+  protected String getList(int[] list) {
+    StringBuilder sb = new StringBuilder();
+    for(int element : list) {
+      sb.append(element + "\n");
+    }
+    return sb.toString();
+  }
+
+  protected int parseUserInput(String input) {
     int userInput = Integer.parseInt(input);
     return userInput;
   }
